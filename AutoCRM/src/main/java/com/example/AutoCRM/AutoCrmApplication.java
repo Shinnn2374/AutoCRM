9bba737695d651623e32b16ace7b68ff04d22352
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-30 20:24:34 +0300

    ...

---
+++ AutoCRM/src/main/java/com/example/AutoCRM/AutoCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.AutoCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCrmApplication.class, args);
+	}
+
+}
---
+++ AutoCRM/src/test/java/com/example/AutoCRM/AutoCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutoCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
